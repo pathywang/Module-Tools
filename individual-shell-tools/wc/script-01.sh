962704607db1b0e4141fbb10a3fad39551758126
pathywang <pathywang@gmail.com>
2026-07-05 10:38:03 +0100

    complete wc

--- individual-shell-tools/wc/script-01.sh
+++ individual-shell-tools/wc/script-01.sh
@@ -2,5 +2,7 @@
 
 set -euo pipefail
 
-# TODO: Write a command to output the number of words in the file helper-files/helper-3.txt.
+# TODO: Write a command  to output the number of words in the file helper-files/helper-3.txt.
 # The output should include the number 19. The output should not include the number 92.
+
+wc -w < ../helper-files/helper-3.txt
