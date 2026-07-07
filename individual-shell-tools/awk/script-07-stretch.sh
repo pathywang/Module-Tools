20b5b4f233ca1829dd685b4b5746fce97121e735
pathywang <pathywang@gmail.com>
2026-07-07 22:50:40 +0100

    update

--- individual-shell-tools/awk/script-07-stretch.sh
+++ individual-shell-tools/awk/script-07-stretch.sh
@@ -7,4 +7,4 @@ set -euo pipefail
 # TODO: Write a command to output just the names of each player along with the total of adding all of that player's scores.
 # Your output should contain 6 lines, each with one word and one number on it.
 # The first line should be "Ahmed 15". The second line should be "Basia 37"
-awk '{sum[$1] += $3} END {for (name in sum) print name, sum[name]}' scores-table.txt
+awk '{for (i=3; i<=NF; i++) sum[$1] += $i} END {for (name in sum) print name, sum[name]}' scores-table.txt
