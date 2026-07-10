39a6f08959d0386f223ff9675cf4aa9ebb3c2cec
pathywang <pathywang@gmail.com>
2026-07-10 17:39:00 +0100

    correction

--- jq/script-03.sh
+++ jq/script-03.sh
@@ -6,4 +6,5 @@ set -euo pipefail
 # TODO: Write a command to output the name of the person, then a comma, then their profession.
 # Your output should be exactly the string "Selma, Software Engineer", but should not contain any quote characters.
 
-jq -r '"\(.name), \(.profession)"' person.json
+jq -r '"\(.name), \(.profession)"' person.json      
+
