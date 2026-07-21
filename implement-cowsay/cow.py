cfebfd188d914f99ac9fae9a94db5469f5614fd5
pathywang <pathywang@gmail.com>
2026-07-21 17:56:22 +0100

    complete course

---
+++ implement-cowsay/cow.py
@@ -0,0 +1,25 @@
+import argparse
+import cowsay
+
+parser = argparse.ArgumentParser(
+    description="Make animals say things"
+)
+
+parser.add_argument(
+    "--animal",
+    choices= cowsay.char_names,      
+    default="cow",
+    help="The animal to be saying things."
+)
+
+parser.add_argument(
+    "message",
+    nargs="+",
+    help="The message to say."
+)
+
+args = parser.parse_args()
+
+print(args.message)
+cowsay.char_funcs[args.animal](" ".join(args.message))
+
