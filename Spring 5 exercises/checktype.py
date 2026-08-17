b1c18c34644ac6041d62cfefb5ad9a8ad30715da
pathywang <pathywang@gmail.com>
2026-08-17 09:07:03 +0100

    add type

--- Spring 5 exercises/checktype.py
+++ Spring 5 exercises/checktype.py
@@ -1,14 +1,14 @@
-def open_account(balances, name, amount):
+def open_account (balances: dict[str, int], name: str, amount: int) -> None:
     balances[name] = amount
 
-def sum_balances(accounts):
+def sum_balances (accounts: dict[str, int]) -> int:
     total = 0
     for name, pence in accounts.items():
         print(f"{name} had balance {pence}")
         total += pence
     return total
 
-def format_pence_as_string(total_pence):
+def format_pence_as_string (total_pence: int) -> str:
     if total_pence < 100:
         return f"{total_pence}p"
     pounds = int(total_pence / 100)
